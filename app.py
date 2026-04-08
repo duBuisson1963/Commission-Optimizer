1e5ad7257df17301ca3ea0e6f7d8a8d92d0fa7de
duBuisson1963 <hannesdubuisson@gmail.com>
2026-04-08 11:04:40 +0200

    Fix display statement formatting in PROFILES

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ PROFILES = {
     "Standard AE / SMME": {
         "statement": {"Radio Classic": 45.0, "TV Classic": 24.0, "TV Sponsorship": 6.0, "Radio Sponsorship": 15.0, "Digital": 5.0, "TV Sport Sponsorship": 2.5, "Radio Sport Sponsorship": 2.5},
         "policy": {"TV Classic": 40.0, "Radio Classic": 30.0, "TV Sponsorship": 10.0, "Radio Sponsorship": 10.0, "Digital": 5.0, "TV Sport Sponsorship": 2.5, "Radio Sport Sponsorship": 2.5},
-        "display_stmt": "45
+    "display_stmt": "45
