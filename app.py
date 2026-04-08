1ac50ca3a12edbf8f2b9f9595af48151c7d66a9b
duBuisson1963 <hannesdubuisson@gmail.com>
2026-04-08 11:05:30 +0200

    Fix formatting in PROFILES dictionary

--- app.py
+++ app.py
@@ -11,5 +11,4 @@ from datetime import datetime
 PROFILES = {
     "Standard AE / SMME": {
         "statement": {"Radio Classic": 45.0, "TV Classic": 24.0, "TV Sponsorship": 6.0, "Radio Sponsorship": 15.0, "Digital": 5.0, "TV Sport Sponsorship": 2.5, "Radio Sport Sponsorship": 2.5},
-        "policy": {"TV Classic": 40.0, "Radio Classic": 30.0, "TV Sponsorship": 10.0, "Radio Sponsorship": 10.0, "Digital": 5.0, "TV Sport Sponsorship": 2.5, "Radio Sport Sponsorship": 2.5},
-    "display_stmt": "45
+        "policy": {"TV Classic": 40.0, "Radio Classic": 30.0, "TV Sponsorship": 10.0, "Radio Sponsorship": 10.0, "Digital": 5.0, "TV Sport Sponsorship": 2.5, "Radio Sport Sponsorship": 2.5}, "display_stmt": "45
